51a0c52497455128e3ec196a8273ffa0e9d03cc0
diegoadrianm <diegomorduar@gmail.com>
2025-12-06 18:35:25 +0100

    change db credentials

--- db/db.js
+++ db/db.js
@@ -3,8 +3,8 @@
 const mysql = require("mysql2");
 const pool = mysql.createPool({
   host: "localhost",
-  user: "diego",
-  password: "diego123",
+  user: "root",
+  password: "",
   database: "aplicaciones_web",
 });
 
