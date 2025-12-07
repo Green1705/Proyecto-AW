67b4ea328817d917a3926e09a5c7597a03fd94fb
diegoadrianm <diegomorduar@gmail.com>
2025-12-07 16:35:20 +0100

    update car status
    updates status

--- routes/formulario_reserva.js
+++ routes/formulario_reserva.js
@@ -10,7 +10,6 @@ router.post("/", (req, res) => {
   const {
     id_usuario,
     id_automovil,
-    id_cliente,
     nombre_cliente,
     apellido_paterno,
     apellido_materno,
@@ -22,35 +21,80 @@ router.post("/", (req, res) => {
     notas,
   } = req.body;
 
-  const query =
-    "INSERT INTO reserva (id_usuario, id_automovil, nombre_cliente, apellido_paterno, apellido_materno, dni, telefono, fecha_inicio, fecha_fin, precio_final, notas) values (?,?,?,?,?,?,?,?,?,?,?,?)";
+  const insertReservaQuery = `
+    INSERT INTO reserva 
+    (id_usuario, id_automovil, nombre_cliente, apellido_paterno, apellido_materno, dni, telefono, fecha_inicio, fecha_fin, precio_final, notas) 
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `;
 
-  pool.query(
-    query,
-    [
-      id_usuario,
-      id_automovil,
-      id_cliente,
-      nombre_cliente,
-      apellido_paterno,
-      apellido_materno,
-      dni,
-      telefono,
-      fecha_inicio,
-      fecha_fin,
-      precio_final,
-      notas,
-    ],
-    (err, result) => {
+  const updateCarQuery = `
+    UPDATE automovil 
+    SET estado = 'reservado'
+    WHERE id_automovil = ?
+  `;
+
+  pool.getConnection((err, connection) => {
+    if (err) {
+      req.flash("error", "Error de conexión con la base de datos");
+      return res.redirect("/vehiculos");
+    }
+
+    connection.beginTransaction((err) => {
       if (err) {
-        req.flash("error", "Error al realizar la reserva");
-        res.redirect("/vehiculos");
-      } else {
-        req.flash("success", "Reserva realizada correctamente");
-        res.redirect("/vehiculos");
+        connection.release();
+        req.flash("error", "No se pudo iniciar la transacción");
+        return res.redirect("/vehiculos");
       }
-    },
-  );
+
+      connection.query(
+        insertReservaQuery,
+        [
+          id_usuario,
+          id_automovil,
+          nombre_cliente,
+          apellido_paterno,
+          apellido_materno,
+          dni,
+          telefono,
+          fecha_inicio,
+          fecha_fin,
+          precio_final,
+          notas,
+        ],
+        (err, result) => {
+          if (err) {
+            connection.rollback(() => {
+              connection.release();
+            });
+            req.flash("error", "Error al crear la reserva");
+            return res.redirect("/vehiculos");
+          }
+
+          connection.query(updateCarQuery, [id_automovil], (err2, result2) => {
+            if (err2) {
+              connection.rollback(() => {
+                connection.release();
+              });
+              req.flash("error", "La reserva no pudo completarse");
+              return res.redirect("/vehiculos");
+            }
+
+            connection.commit((err3) => {
+              connection.release();
+
+              if (err3) {
+                req.flash("error", "Error al confirmar la transacción");
+                return res.redirect("/vehiculos");
+              }
+
+              req.flash("success", "Reserva realizada correctamente");
+              return res.redirect("/vehiculos");
+            });
+          });
+        },
+      );
+    });
+  });
 });
 
 module.exports = router;
