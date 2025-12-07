300bb186eceaf79d32c2b94dc62d6372d50094c4
diegoadrianm <diegomorduar@gmail.com>
2025-12-07 10:49:39 +0100

    login redirect
    redirect to login form

--- middlewares/is_logged_in.js
+++ middlewares/is_logged_in.js
@@ -4,7 +4,7 @@ function is_logged(req, res, next) {
   if (req.session && req.session.isLoggedIn) {
     next();
   } else {
-    res.json({ error: "Sesión no iniciada" });
+    res.redirect("/login");
   }
 }
 
