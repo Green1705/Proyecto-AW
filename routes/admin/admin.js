ac2393fbd6c0fb347d7af3dbfd36654612fc91f4
Green1705 <gorastgor@gmail.com>
2025-12-07 18:26:25 +0100

    adasd

--- routes/admin/admin.js
+++ routes/admin/admin.js
@@ -5,9 +5,10 @@ const isAdmin = require("../../middlewares/is_admin.js");
 
 const router = express.Router();
 
-router.use("/", isAdmin, (req, res) => {
+router.get("/", isAdmin, (req, res) => {
   res.render("admin_panel");
 });
+
 router.use("/vehiculos", isAdmin, require("./cars.js"));
 router.use("/concesionarios", isAdmin, require("./dealership.js"));
 router.use("/empleados", isAdmin, require("./employee.js"));
