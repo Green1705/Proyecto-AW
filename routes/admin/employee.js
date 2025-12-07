39dc4b7a55a1ddf2ad75a78f46b7356837294866
diegoadrianm <diegomorduar@gmail.com>
2025-12-07 11:36:54 +0100

    employee insert
    insert employee into database

--- routes/admin/employee.js
+++ routes/admin/employee.js
@@ -13,12 +13,52 @@ router
       if (err) {
         res.status(500).json({ message: err });
       } else {
-        res.render("forms/employee_form", { employees: results });
+        res.render("forms/employee_form", {
+          employees: results,
+          success: req.flash("success"),
+          error: req.flash("error"),
+        });
       }
     });
   })
   .post((req, res) => {
     //TODO add a new employee to the database
+    const {
+      nombre,
+      apellido_paterno,
+      apellido_materno,
+      rol,
+      email,
+      password,
+      telefono,
+      id_concesionario,
+    } = req.body;
+
+    const query =
+      "INSER INTO usuario (nombre, apellido_paterno, apellido_materno, rol, email, password, telefono, id_concesionario) values (?,?,?,?,?,?,?,?)";
+
+    pool.query(
+      query,
+      [
+        nombre,
+        apellido_paterno,
+        apellido_materno,
+        rol,
+        email,
+        password,
+        telefono,
+        id_concesionario,
+      ],
+      (err, result) => {
+        if (err) {
+          req.flash("error", "Error al crear empleado");
+          res.redirect("/admin/empleados");
+        } else {
+          req.flash("success", "Empleado agregado con éxito");
+          res.redirect("/admin/empleados");
+        }
+      },
+    );
   });
 
 module.exports = router;
