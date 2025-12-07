f41cc6a40881c574023468093200c57f2bbb7e12
Green1705 <gorastgor@gmail.com>
2025-12-07 23:41:03 +0100

    dde

--- routes/reservas_activas.js
+++ routes/reservas_activas.js
@@ -6,7 +6,12 @@ const pool = require("../db/db.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const query = "SELECT * FROM reserva WHERE estado = 'activa' ";
+  const query = `
+    SELECT r.*, a.matricula
+    FROM reserva AS r
+    LEFT JOIN automovil AS a ON r.id_automovil = a.id_automovil
+    WHERE r.estado = 'activa'
+  `;
 
   pool.query(query, (err, result) => {
     if (err) {
