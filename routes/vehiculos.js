c61885e9eebe0a3e226b788e629c5f8fe7c032d4
diegoadrianm <diegomorduar@gmail.com>
2025-12-07 11:52:04 +0100

    car insert
    insert new car into the database

--- routes/vehiculos.js
+++ routes/vehiculos.js
@@ -13,11 +13,58 @@ router
       if (err) {
         res.status(500).json({ message: err });
       } else {
-        res.render("vehiculos", { cars: result });
+        res.render("vehiculos", {
+          cars: result,
+          success: req.flash("success"),
+          error: req.flash("error"),
+        });
       }
     });
   })
-  .post((req, res) => {});
+  .post((req, res) => {
+    const {
+      matricula,
+      marca,
+      modelo,
+      anio_matriculacion,
+      numero_plazas,
+      autonomia_km,
+      precio_por_dia,
+      color,
+      imagen,
+      estado,
+      id_concesionario,
+    } = req.body;
+
+    const query =
+      "INSERT INTO automovil (matricula, marca, modelo, anio_matriculacion, numero_plazas, autonomia_km, precio_por_dia, color, imagen, estado, id_concesionario) VALUES (?,?,?,?,?,?,?,?,?,?,)";
+
+    pool.query(
+      query,
+      [
+        matricula,
+        marca,
+        modelo,
+        anio_matriculacion,
+        numero_plazas,
+        autonomia_km,
+        precio_por_dia,
+        color,
+        imagen,
+        estado,
+        id_concesionario,
+      ],
+      (err, result) => {
+        if (err) {
+          req.flash("error", "Error al crear vehiculo");
+          res.redirect("/admin/vehiculos");
+        } else {
+          req.flash("success", "Vehiculo creado correctamente");
+          res.redirect("/admin/vehiculos");
+        }
+      },
+    );
+  });
 
 // router.get("/:id", (req, res) => {
 //   res.render("users/profile", { vehiculoId: req.params.id });
