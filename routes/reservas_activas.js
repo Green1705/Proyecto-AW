162bf572a6e196c7d144a5a2fd7b47cfe7320bcb
diegoadrianm <diegomorduar@gmail.com>
2025-12-07 17:54:42 +0100

    correct query

--- routes/reservas_activas.js
+++ routes/reservas_activas.js
@@ -6,12 +6,13 @@ const pool = require("../db/db.js");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  const query = "SELECT * FROM automovil WHERE estado = 'activa' ";
+  const query = "SELECT * FROM reserva WHERE estado = 'activa' ";
 
   pool.query(query, (err, result) => {
     if (err) {
       res.status(500).json({ message: "Error al consultar la base de datos" });
     } else {
+      console.log(result);
       res.render("reservas_activas", { reservations: result });
     }
   });
